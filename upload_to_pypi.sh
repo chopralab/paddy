fd589eef4616618df8a97265b05e66b3db02c08c
Jonathan Fine <finej@purdue.edu>
2019-12-02 13:58:54 -0500

    Version 0.2

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -4,6 +4,8 @@ echo -e "[pypi]" >> ~/.pypirc
 echo -e "username = chopralab" >> ~/.pypirc
 echo -e "password = $PYPI_PASSWORD" >> ~/.pypirc
 
+python setup.py install --user
+
 pip install twine
 twine upload dist/*
 
