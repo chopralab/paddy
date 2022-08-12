79e90dfe4fe0f93e571ef6c718330d78edf6ea17
Armen <52046521+armenbeck@users.noreply.github.com>
2022-08-12 13:13:21 -0400

    Update upload_to_pypi.sh

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "[pypi]" >> ~/.pypirc
-echo -e "username = armenbeck" >> ~/.pypirc
+echo -e "username = chopralab" >> ~/.pypirc
 echo -e "password = $PYPI_PASSWORD" >> ~/.pypirc
 
 python setup.py install --user
