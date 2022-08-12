7ad26797727c11a5e9669cdc7e23786146c4cd22
Armen <52046521+armenbeck@users.noreply.github.com>
2022-08-12 15:24:36 -0400

    Update upload_to_pypi.sh

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo -e "[pypi]" >> ~/.pypirc
+echo -e "[pypi]" > ~/.pypirc
 echo -e "username = chopralab" >> ~/.pypirc
 echo -e "password = ${{PYPI.TEST}}" >> ~/.pypirc
-echo ${{PYPI.TEST}}
-
-python setup.py install --user
-
-pip install twine
-twine upload dist/*
+echo ${PYPI.TEST}
 
