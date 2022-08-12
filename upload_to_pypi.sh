10a9c0021299a09a71b0c32343c8eb857feae5a9
Armen <52046521+armenbeck@users.noreply.github.com>
2022-08-12 13:09:50 -0400

    Update upload_to_pypi.sh

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -7,6 +7,6 @@ echo -e "password = $PYPI_PASSWORD" >> ~/.pypirc
 python setup.py install --user
 
 pip install twine
-twine upload dist/*
+twine --verbose upload dist/*
 #twine upload --verbose -u __token__ -p $PYPI_PASSWORD dist dist/*
 
