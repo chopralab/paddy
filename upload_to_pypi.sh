59883032d5fbd0bd80a518e88027c663886d7020
Armen <52046521+armenbeck@users.noreply.github.com>
2022-08-15 10:29:03 -0400

    Update upload_to_pypi.sh

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -5,7 +5,12 @@ echo -e "username = __token__" >> ~/.pypirc
 #echo -e "password = ${{ secrets.PYPI_API_TOKEN }}" >> ~/.pypirc
 echo -e "password = $API_TOKEN" >> ~/.pypirc
 
+echo -e "[testpypi]" >> ~/.pypirc
+echo -e "username = armenbeck" >> ~/.pypirc
+#echo -e "password = $API_TOKEN" >> ~/.pypirc
+
 python setup.py install --user
 
 pip install twine
+twine upload --repository testpypi dist/*
 twine upload dist/*
