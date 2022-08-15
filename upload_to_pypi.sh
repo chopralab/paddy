9612b1692522195b56c250f2dbd28068441f626d
Armen <52046521+armenbeck@users.noreply.github.com>
2022-08-15 10:41:22 -0400

    Update upload_to_pypi.sh

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -6,7 +6,7 @@ echo -e "username = __token__" >> ~/.pypirc
 echo -e "password = $API_TOKEN" >> ~/.pypirc
 
 echo -e "[testpypi]" >> ~/.pypirc
-echo -e "username = armenbeck" >> ~/.pypirc
+echo -e "username = __token__" >> ~/.pypirc
 echo -e "password = $TEST_TOKEN" >> ~/.pypirc
 
 python setup.py install --user
